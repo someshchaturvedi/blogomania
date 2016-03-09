e41e7f934d809f2c1996be26c62ce6418448b83d
hulksmash <somesh.08.96@gmail.com>
2016-03-09 19:41:23 +0530

    logo

--- blog/views.py
+++ blog/views.py
@@ -107,12 +107,6 @@ def save_blog(request):
 
    		def get_api(cfg):
   			graph = facebook.GraphAPI(cfg['access_token'])
-  			#resp = graph.get_object('me/accounts')
-			#page_access_token = None
-			#for page in resp['data']:
-			#	if page['id'] == cfg['page_id']:
-			#		page_access_token = page['access_token']
-			#graph = facebook.GraphAPI(page_access_token)
 			return graph
 
   		api = get_api(cfg)
